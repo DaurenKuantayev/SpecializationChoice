78f5474a7c15a5a549a0f51ef8b5a4ec6cb773ac
DaurenKuantayev <52776105+DaurenKuantayev@users.noreply.github.com>
2023-01-01 14:50:47 +0600

    Решение задачи

--- Program.cs
+++ Program.cs
@@ -1,25 +1,28 @@
-﻿System.Console.WriteLine("Введите размер массива");
-int sizeArr = Convert.ToInt32(Console.ReadLine());
-string[] arrey = new string[sizeArr];
-var rand = new Random();
-for (int i = 0; i < arrey.Length; i++)
+int ArrayQuantity(string[] array, int elemLength) // Создаем метод поиска количества элементов в массиве которые подходят по условию задачи
 {
-    int stringSize = rand.Next(1, 5);
-    for (int j = 0; j < stringSize; j++)
+    int quantity = 0;
+    for (int i = 0; i < array.Length; i++)
     {
-        arrey[i] += (char)rand.Next('a', 'z');
+        if (array[i].Length <= elemLength) quantity++;
+    }
+    return quantity;
+}
+void ArrayTransferringElements(string[] array, string[] arrayTransfer, int elemLength) // метод для переноса значений которые подходят по условию задачи в новый массив
+{
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= elemLength)
+        {
+            arrayTransfer[j] = array[i];
+            j++;
+        }
     }
 }
-System.Console.WriteLine("[" + string.Join(", ", arrey) + "]");
-int sizeSecondArr = 0;
-string[] arrThreeSize = new string[sizeSecondArr];
-for (int i = 0; i < arrey.Length; i++)
-{
-    if (arrey[i].Length == 3)
-    {
-        sizeSecondArr++;
-        Array.Resize(ref arrThreeSize, sizeSecondArr);
-        arrThreeSize[sizeSecondArr - 1] = arrey[i];
-    }
-}
-System.Console.WriteLine("[" + string.Join(", ", arrThreeSize) + "]");
+int requiredLengthElement = 3;
+System.Console.WriteLine("Введите элементы массива через точку с запятой\n Пример => hello;2;world;=)");
+string[] arrayString = Array.ConvertAll<string, string>(Console.ReadLine().Split(';'), elem => { return System.Convert.ToString(elem); });
+int quantityElems = ArrayQuantity(arrayString, requiredLengthElement);
+string[] arraySelectedValues = new string[quantityElems];
+ArrayTransferringElements(arrayString, arraySelectedValues, requiredLengthElement);
+System.Console.WriteLine(string.Join(" ; ", arraySelectedValues));
